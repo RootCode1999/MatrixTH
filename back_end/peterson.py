f7516575ad61b1e6c4548fca13dc064c86675304
Matrix-Li <lky1901246487@gmail.com>
2023-03-12 11:05:43 +0800

    Create peterson.py

---
+++ back_end/peterson.py
@@ -0,0 +1,26 @@
+class peterson:
+    flag = [False, False]
+    turn = 1
+
+    def returnfalg0():
+        return self.flag[0]
+
+    def returnfalg1():
+        return self.flag[1]
+
+    def returnturn():
+        return self.turn
+
+    def changflag0(a):
+        self.flag[0] = a
+
+    def changflag1(a):
+        self.flag[1] = a
+
+    def changeturn(a):
+        self.turn = a
+
+    def __init__():
+        self.flag[0] = False
+        self.flag[1] = False
+        self.turn = 1
